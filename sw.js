31675bd3e54743c3ff1903def54adb7547689e40
Rahul Kadyan <hi@znck.me>
2017-06-23 20:51:14 +0530

    Fix bug in api

--- sw.js
+++ sw.js
@@ -19,20 +19,20 @@ importScripts('workbox-sw.prod.v1.0.1.js');
  */
 const fileManifest = [
   {
-    "url": "/_nuxt/0.nuxt.bundle.37b4c1a8ac481f4aeb46.js",
-    "revision": "f998e197883c591fc6ebd2d2aadca96f"
+    "url": "/_nuxt/0.nuxt.bundle.7d453f20c4f47ca59715.js",
+    "revision": "d57bbea32d815c272943fbe78099ca8e"
   },
   {
-    "url": "/_nuxt/1.nuxt.bundle.6871005028dac2acd854.js",
-    "revision": "05b7bf44fe9a14cefa83031dddaf5fcb"
+    "url": "/_nuxt/1.nuxt.bundle.8e6cd042c754d0dc6c41.js",
+    "revision": "9113fa7f1ebf7562f20a1e1551296974"
   },
   {
-    "url": "/_nuxt/2.nuxt.bundle.a36c0f46a74fe9980d01.js",
-    "revision": "f508718de93e87aada111502269d2796"
+    "url": "/_nuxt/2.nuxt.bundle.fb201e5fe5626ccf96f6.js",
+    "revision": "6025413eaccf29c013f3bf4c24765b19"
   },
   {
-    "url": "/_nuxt/3.nuxt.bundle.5a589084326f9bb6f4e4.js",
-    "revision": "5ab42d1ca5ddb25048a8973434cc60ff"
+    "url": "/_nuxt/3.nuxt.bundle.185a2db0d3834a9ec9cb.js",
+    "revision": "11fc8aa368a9918cc734399f6e028621"
   },
   {
     "url": "/_nuxt/4.nuxt.bundle.4e723778c19b7f81600b.js",
@@ -100,27 +100,27 @@ const fileManifest = [
   },
   {
     "url": "/_nuxt/client-manifest.json",
-    "revision": "e63504e0fdb3b234e1443daafd8c911c"
+    "revision": "4db303dfd3ac18d784cc753cb65b6eba"
   },
   {
     "url": "/_nuxt/index.html",
     "revision": "744fb1c184ce0e85af057191b683d6a2"
   },
   {
-    "url": "/_nuxt/manifest.f99191f7b03a7be35a7e.js",
-    "revision": "64b6ca44d38ea5c55e0b87e07e526479"
+    "url": "/_nuxt/manifest.044c10774f31d789d596.js",
+    "revision": "db2443347b7cf27129662d0bbc602e30"
   },
   {
-    "url": "/_nuxt/nuxt.bundle.555099c08fe076e3c291.js",
-    "revision": "6ce5cdfe3523406960bd329db63ccfb4"
+    "url": "/_nuxt/nuxt.bundle.460778f6f58f6ae8415d.js",
+    "revision": "3d8e1ffbb9de742237086b4cdc539962"
   },
   {
     "url": "/_nuxt/server-bundle.json",
-    "revision": "0eb8c74f740e800763fbc14059d39eba"
+    "revision": "3f580e837cabae0c231d40f4fe356d7d"
   },
   {
-    "url": "/_nuxt/vendor.bundle.e55b17f23472c056ccb1.js",
-    "revision": "d260d9ee283a4daca9a7dda884c67460"
+    "url": "/_nuxt/vendor.bundle.4ea82008ba94f7dd76fe.js",
+    "revision": "8a345839fc9f78125da1756515513119"
   }
 ];
 
